bbbe72942abb08708c5b1ca666aeb8f4afd8e6a0
Federico Botta <fedebot@gmail.com>
2016-12-28 10:27:15 +0100

    altri punti ita

--- app/scripts/controllers/carte.js
+++ app/scripts/controllers/carte.js
@@ -52,6 +52,62 @@ var data = {
     imgF0: "files/ampere/0616pwt_F0_AI.jpg",
     audioTa: "files/ampere/0616pwta0_ton.wav",
     audioTi: "files/ampere/0616pwti0_ton.wav"
+  },
+  parabita2 : {
+    frase: "La papera mangia la patata",
+    trascrizione: "[la ˈpapera ˈmanʤa la paˈtata]",
+    imgDur: "files/ampere/0621pwt_dur_AI.jpg",
+    imgF0: "files/ampere/0621pwt_F0_AI.jpg",
+    audioTa: "files/ampere/0621pwta0_ton.wav",
+    audioTi: "files/ampere/0621pwti0_ton.wav"
+  },
+  arezzo : {
+    frase: "La papera mangia la patata",
+    trascrizione: "[la ˈpapera ˈmanʤa la paˈtata]",
+    imgDur: "files/ampere/0707pwt_dur_AI.jpg",
+    imgF0: "files/ampere/0707pwt_F0_AI.jpg",
+    audioTa: "files/ampere/0707pwta0_ton.wav",
+    audioTi: "files/ampere/0707pwti0_ton.wav"
+  },
+  cagliari : {
+    frase: "La papera mangia la patata",
+    trascrizione: "[la ˈpapera ˈmanʤa la paˈtata]",
+    imgDur: "files/ampere/06j8pwt_dur_AI.jpg",
+    imgF0: "files/ampere/06j8pwt_F0_AI.jpg",
+    audioTa: "files/ampere/06j8pwta0_ton.wav",
+    audioTi: "files/ampere/06j8pwti0_ton.wav"
+  },
+  melia : {
+    frase: "La papera mangia la patata",
+    trascrizione: "[la ˈpapera ˈmanʤa la paˈtata]",
+    imgDur: "files/ampere/0737pwt_dur_AI.jpg",
+    imgF0: "files/ampere/0737pwt_F0_AI.jpg",
+    audioTa: "files/ampere/0737pwta0_ton.wav",
+    audioTi: "files/ampere/0737pwti0_ton.wav"
+  },
+  pollina : {
+    frase: "La papera mangia la patata",
+    trascrizione: "[la ˈpapera ˈmanʤa la paˈtata]",
+    imgDur: "files/ampere/0724pwt_dur_AI.jpg",
+    imgF0: "files/ampere/0724pwt_F0_AI.jpg",
+    audioTa: "files/ampere/0724pwta0_ton.wav",
+    audioTi: "files/ampere/0724pwti0_ton.wav"
+  },
+  taranto : {
+    frase: "La papera mangia la patata",
+    trascrizione: "[la ˈpapera ˈmanʤa la paˈtata]",
+    imgDur: "files/ampere/06b6pwt_dur_AI.jpg",
+    imgF0: "files/ampere/06b6pwt_F0_AI.jpg",
+    audioTa: "files/ampere/06b6pwta0_ton.wav",
+    audioTi: "files/ampere/06b6pwti0_ton.wav"
+  },
+  verona : {
+    frase: "La papera mangia la patata",
+    trascrizione: "[la ˈpapera ˈmanʤa la paˈtata]",
+    imgDur: "files/ampere/06i6pwt_dur_AI.jpg",
+    imgF0: "files/ampere/06i6pwt_F0_AI.jpg",
+    audioTa: "files/ampere/06i6pwta0_ton.wav",
+    audioTi: "files/ampere/06i6pwti0_ton.wav"
   }
 };
 
